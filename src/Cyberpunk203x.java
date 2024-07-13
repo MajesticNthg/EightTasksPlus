0a557d5d6b305312b6638066d40798edfe4b33ca
Danil <147924429+MajesticNthg@users.noreply.github.com>
2024-07-13 23:10:56 +1000

    Create project 3. Cyberpunk 203x - 1990

---
+++ src/Cyberpunk203x.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class Cyberpunk203x {
+    public static boolean ECC_help (int[] arr1, int[] arr2) {
+        if (arr1.length != arr2.length) return false;
+
+        HashMap<Integer, Integer> result1 = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> result2 = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> result3 = new HashMap<Integer, Integer>();
+
+        for (int x = 0; x < arr1.length; x++) {
+            result1.put(arr1[x], x);
+            result3.put(arr2[x], x);
+            result3.put(arr1[x], x);
+            result2.put(arr2[x], x);
+        }
+        if (result1.size() != result3.size() || result2.size() != result3.size()) return false;
+
+        return true;
+    }
+}
+
