d49b98961626579b28e318cbd7faaf5647ceb253
Danil <147924429+MajesticNthg@users.noreply.github.com>
2024-07-31 23:44:05 +1000

    Update Cyberpunk203xPt6FinalBoss
    Changed with a reference solution second method. Create a new massive with a sum digits and use it in the new method.

---
+++ src/Cyberpunk203xPt6FinalBoss_UPD.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+
+public class Cyberpunk203xPt6FinalBoss_UPD {
+    public static String army_communication_matrix(int n, int[][] matrix) {
+        int max_sum = 0;
+        int sum = 0;
+        int size = 2;
+        int[] enter_parameters = new int[3];
+        int[] correct_parameters = new int[3];
+        int[][] subMatrix = new int[matrix.length + 1][matrix[0].length + 1];
+        create_subMatrix(matrix, subMatrix);
+
+        while (size < n) {
+            calculation(n, max_sum, enter_parameters, correct_parameters, subMatrix, size, sum);
+            size++;
+        }
+
+        return Arrays.toString(correct_parameters);
+    }
+    public static int[][] create_subMatrix (int[][] matrix, int[][] subMatrix) {
+        for (int x = 0; x < matrix.length; x++) {
+            for (int y = 0; y < matrix[0].length; y++) {
+                if (y == 0) {
+                    subMatrix[x + 1][y + 1] = matrix[x][y];
+                }
+                else {
+                    subMatrix[x + 1][y + 1] = matrix[x][y] + subMatrix[x + 1][y];
+                }
+            }
+        }
+
+        return subMatrix;
+    }
+    public static int sum_subMatrix (int max_sum, int[] enter_parameters, int[] correct_parameters, int[][] subMatrix, int size, int x, int y, int sum) {
+        enter_parameters[0] = x;
+        enter_parameters[1] = y;
+        enter_parameters[2] = size;
+        for (int i = 0; i < size; i++, y++) {
+            sum += subMatrix[y + 1][x + size] - subMatrix[y + 1][x];
+        }
+        if (sum >= max_sum) {
+        max_sum = sum;
+        System.arraycopy(enter_parameters, 0, correct_parameters, 0, 3);
+    }
+        return max_sum;
+    }
+    public static int[] calculation (int n, int max_sum, int[] enter_parameters, int[] correct_parameters, int[][] subMatrix, int size, int sum) {
+        for (int x = 0; x + size <= n; x++) {
+            for (int y = 0; y + size <= n; y++) {
+                max_sum = sum_subMatrix(max_sum, enter_parameters, correct_parameters, subMatrix, size, x, y, sum);
+            }
+        }
+
+        return correct_parameters;
+    }
+}
